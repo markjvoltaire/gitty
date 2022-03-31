69d44b2f0e03be64f4e82cc927decbf8a6847d5f
Mark Voltaire <markjvoltaire@gmail.com>
2022-03-31 15:10:51 -0400

    wrote test to direct user to github oauth

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,4 +11,12 @@ describe('gitty routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('should redirect to the github oauth page upon login', async () => {
+    const req = await request(app).get('/api/v1/github/login');
+
+    expect(req.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
+    );
+  });
 });
